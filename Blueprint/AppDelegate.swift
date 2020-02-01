9ed092c98a9f764e5739daac171768bbdd2662f1
Samuel Proulx <samuelproulx26@gmail.com>
2020-01-31 19:23:04 -0500

    Initial Commit

---
+++ Blueprint/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Blueprint
+//
+//  Created by Samuel Proulx on 2020-01-31.
+//  Copyright © 2020 Blueprint Technologies Inc. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Blueprint/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Blueprint
+//
+//  Created by Samuel Proulx on 2020-01-31.
+//  Copyright © 2020 Blueprint Technologies Inc. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
